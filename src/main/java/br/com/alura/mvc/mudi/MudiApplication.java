4ba4476a79318c5b3b10d4ea160452db2f323dad
Marcio-Rodrigues <mrfKaleo@gmail.com>
2020-10-10 21:52:01 -0300

    completo ate a aula 5

--- src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
+++ src/main/java/br/com/alura/mvc/mudi/MudiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MudiApplication {
 
 	public static void main(String[] args) {
+		System.out.println("falta a aula 6 e 7");
 		SpringApplication.run(MudiApplication.class, args);
 	}
 
---
+++ src/main/java/rr/com/alura/mvc/mudi/dto/RequisicaoNovoPedido.java
@@ -0,0 +1,55 @@
+package rr.com.alura.mvc.mudi.dto;
+
+import javax.validation.constraints.NotBlank;
+
+import br.com.alura.mvc.mudi.model.Pedido;
+
+public class RequisicaoNovoPedido {
+// anotação do bean validation 
+	@NotBlank
+	private String nomeProduto;
+	
+	@NotBlank
+	private String urlProduto;
+	
+	@NotBlank
+	private String urlImagem;
+	
+	
+	private String descricao;
+	
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	
+	public Pedido toPedido() {
+		Pedido pedido = new Pedido();
+		pedido.setNomeProduto(nomeProduto);
+		pedido.setDescricao(descricao);
+		pedido.setUrlImagem(urlImagem);
+		pedido.setUrlProduto(urlProduto);
+		
+		return pedido;
+	}
+}
